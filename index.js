c2d4495d330e00b03d1c45602ba5d3e4236c0b1d
Betelhem Geremew <103342290+betelhem345@users.noreply.github.com>
2022-08-17 04:59:42 +0000

    test

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine' , 'ejs')
+
+app.get('/', (req, res) => {
+
+  var title = 'Our Home Page';
+  var heading = 'My Website';
+  res.render('pages/index',{
+    'title':title,
+    'heading': heading
+  });
+})
+
+app.get('/about', (req, res) => {
+
+  var title = 'Our about Page';
+  var heading = 'My Website';
+  res.render('pages/about',{
+    'title':title,
+    'heading': heading
+  });
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

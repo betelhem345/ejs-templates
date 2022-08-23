c385ef74016522cc97265071d1300d1e3ce34306
Betelhem Geremew <103342290+betelhem345@users.noreply.github.com>
2022-08-23 03:00:25 +0000

    added file

--- index.js
+++ index.js
@@ -2,27 +2,64 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.set('view engine' , 'ejs')
+var data = require('./data/test.json');
 
-app.get('/', (req, res) => {
+app.set('view engine','ejs')
 
-  var title = 'Our Home Page';
-  var heading = 'My Website';
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+app.use("/images", express.static(__dirname));
+
+
+app.get('/', (req, res) =>{ 
+  let title = 'Welcome to My Page'
+  let heading = 'Betelhem Shenkut'
   res.render('pages/index',{
     'title':title,
-    'heading': heading
-  });
+    'heading':heading
+  })
 })
-
-app.get('/about', (req, res) => {
-
-  var title = 'Our about Page';
-  var heading = 'My Website';
+app.get('/about', (req, res) =>{ 
+  let title = 'About ME'
+  let heading = 'Aspiring UX/UI designer'
   res.render('pages/about',{
     'title':title,
-    'heading': heading
-  });
+    'heading':heading
+  })
 })
+
+app.get('/users', (req, res) =>{ 
+  let title = 'My user page'
+  let heading = 'My web page '
+  res.render('users/index',{
+    'title':title,
+    'heading':heading,
+    'users': data,
+  })
+})
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ var heading = "My website"
+ res.render('users/view', {
+     title: title,
+     heading:heading,
+     user: data[--id]
+ });
+});
+app.get('/hobby', (req, res) =>{ 
+  let title = 'Hobbies'
+  let heading = 'Running'
+  res.render('pages/hobby',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(data);
 })
